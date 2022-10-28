3a4700a2d1c98db135d112a327d0e2a7f6616e90
Samuel Chavez <sa5m.escom@gmail.com>
2022-10-28 00:32:05 -0500

    call route

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from posti_core.posti import Posti
 import json
 
-
 app = Flask(__name__)
+
+posti = Posti()
 
 
 @app.route("/")
 def home():
-    posti = Posti()
 
+    return render_template("index.html")
+
+
+@app.route("/call", methods=["POST", "GET"])
+def call_posti():
+    if request.method == "POST":
+        data_json = request.get_json()
+        res = posti.call_request(
+            data_json.get("method"),
+            data_json.get("url"),
+            data_json.get("params"),
+            data_json.get("json"),
+            data_json.get("headers")
+        )
+        return res
+
+    return {
+        "message": "no validate"
+    }
+
+
+@app.route("/example")
+def example_from_file():
     with open("example.json", encoding='utf-8') as f:
         data_file = f.read()
         f.close()
@@ -23,4 +46,9 @@ def home():
         data.get("json"),
         data.get("headers")
     )
-    return str(res)
+    return res
+
+
+if __name__ == "__main__":
+    # app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
